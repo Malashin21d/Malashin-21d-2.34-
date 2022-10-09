9a5e3e99334c3f2da7065cac389020fccd79f901
Сергей <@ANDREYNOTEBOOK>
2022-10-09 17:42:51 +0300

    Добавьте файлы проекта.

---
+++ Malashin-21d 2.34/Program.cs
@@ -0,0 +1,12 @@
+﻿int a1, a2, b,res1, res2,res3,res;
+Console.WriteLine("Введите превую цифру из двухзанчнго ");
+a2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую цифру из двухзанчнго ");
+a1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите цифру однозачнаю ");
+b = int.Parse(Console.ReadLine());
+res = a2 + a1 + b;
+res1 = res / 10;
+    res2 = res % 10;
+Console.WriteLine("Ответ {0},{1}",res1,res2);
+
